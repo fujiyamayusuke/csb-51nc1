6344d9b014c4d7975db6d4af4259ac5b7628affd
Code Sandbox <hello@codesandbox.io>
2021-05-28 23:05:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d326b651daab768205ab5369ba116b2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016e7fca228b671fb5fc5ac8e0327c29",
+    "url": "https://fujiyamayusuke.github.io/csb-51nc1/index.html"
+  },
+  {
+    "revision": "0cac7ff237f5ae1f661c",
+    "url": "https://fujiyamayusuke.github.io/csb-51nc1/static/css/main.c34a4f03.chunk.css"
+  },
+  {
+    "revision": "2a6c9f61dc86bea53864",
+    "url": "https://fujiyamayusuke.github.io/csb-51nc1/static/js/2.cd4588db.chunk.js"
+  },
+  {
+    "revision": "0cac7ff237f5ae1f661c",
+    "url": "https://fujiyamayusuke.github.io/csb-51nc1/static/js/main.f5d6b1c7.chunk.js"
+  },
+  {
+    "revision": "9af1392e9096f73cfc95",
+    "url": "https://fujiyamayusuke.github.io/csb-51nc1/static/js/runtime~main.3ff17353.js"
+  }
+]);
